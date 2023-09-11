eb7cd85a7298761545ff9eea32ef5f949200ca6c
Pustovalov Grigoriy <91904869+gp8stvlv@users.noreply.github.com>
2023-09-11 14:40:51 +0300

    lab1 done

---
+++ chemistry_prod/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for chemistry_prod project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from chemistry_prod.views import GetTypesAnalysisPage
+from chemistry_prod.views import GetModelingDetailedPage
+from chemistry_prod.views import GetBasket
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', GetTypesAnalysisPage),
+    path('order/<int:id>/', GetModelingDetailedPage, name='order_url'),     #параметр name='order_url' - передаем в orders.html <a href="{% url 'order_url' model.id %}"
+    path('basket', GetBasket)
+]
---
+++ chemistry_prod/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from datetime import date
+# controller
+# mvt - view = contoller
+
+data_modeling = {
+    'modeling': [
+        {
+            'id': 0,
+            'type': 'Спектрофотометр Nano-500',
+            'description': 'Спектрофотометр и флуориметр в одном приборе. Измерения в малых объемах в диапазоне длин волн 200 – 800 нм.',
+            'image_url': '../static/images/spektrofotometr-nano-500.png',
+            'price': '99$'
+            
+        },
+        {
+            'id': 1,
+            'type': 'Автоклав с вертикальной загрузкой MVS-83, объем 75 литров',
+            'description': 'Паровой стерилизатор с вертикальной загрузкой с температурой стерилизации 115 - 135°C.',
+            'image_url': '../static/images/avtoklav-s-vertikalnoy-zagruzkoy-mvs-83-obem-75-litrov.jpg',
+            'price': '49$'
+        },
+        {
+            'id': 2,
+            'type': 'Низкотемпературный морозильник MDF-U880VH',
+            'description': 'Вертикальный низкотемпературный морозильник MDF-U880VH объемом 861 л с температурой от -50°С до -86°С.',
+            'image_url': '../static/images/nizkotemperaturnyy-morozilnik-mdf-u880vh.jpg',
+            'price': '39$'
+        },
+        {
+            'id': 3,
+            'type': 'Гомогенизатор лабораторный Bioprep-24R с функцией охлаждения на 24 образца',
+            'description': 'Гомогенизатор с функцией активного охлаждения на 24 образца объемом 2 мл или на 12 образцов объемом 5 мл.',
+            'image_url': '../static/images/gomogenizator-laboratornyy-bioprep-24r-s-funktsiey-okhlazhdeniya-na-24-obraztsa.jpg',
+            'price': '129$'
+        },
+        {
+            'id': 4,
+            'type': 'Планшетный спектрофотометр FlexA-200HT',
+            'description': 'Планшетный ридер и кюветный спектрофотометр для видимой и УФ областей спектра.',
+            'image_url': '../static/images/planshetnyy-spektrofotometr-flexa-200ht.jpg',
+            'price': '89$'
+        },
+        {
+            'id': 5,
+            'type': 'Микроцентрифуга M1324',
+            'description': 'Микроцентрифуга со скоростью вращения до 15000 об/мин, с возможностью работы со стандартными пробирками на 1,5/2 мл и ПЦР-стрипами.',
+            'image_url': '../static/images/fluorimetr-fluo-800-siniy-i-krasnyy-kanaly.jpg',
+            'price': '139$'
+        },
+    ]
+}
+
+def GetTypesAnalysisPage(request):
+    return render(request, 'orders.html', {
+        'init_data' : data_modeling
+    })
+
+
+def GetModelingDetailedPage(request, id):
+    data_by_id = data_modeling.get('modeling')[id]
+    return render(request, 'order.html', {
+        'modeling': data_by_id
+    })
+
+def GetBasket(request): #корзина
+    return render(request, 'basket.html')
